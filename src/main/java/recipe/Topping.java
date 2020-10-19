02d93784ee6cd3127177233e80df289cc996897b
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-10-19 11:11:09 +0200

    class topping

--- src/main/java/recipe/Topping.java
+++ src/main/java/recipe/Topping.java
@@ -1,4 +1,16 @@
 package java.recipe;
 
 public class Topping {
+    private String type;
+
+    public Topping(String type){
+        this.type = type;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
