e77392d7e7b38b166eeb9589cc335c444dce5e30
ChenShuangshuang77 <chenss961214@gmail.com>
2020-10-19 11:19:01 +0200

    #1 create Dough class and Flavour class.

--- src/main/java/recipe/Flavour.java
+++ src/main/java/recipe/Flavour.java
@@ -1,4 +1,17 @@
 package java.recipe;
 
 public class Flavour {
+    private String type;
+
+    public Flavour(String type){
+        this.type = type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
