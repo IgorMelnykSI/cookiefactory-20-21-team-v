8ae3330b6214817d6d0f81328831297edf73a865
YUQIAO ZHAO <zhaoyuqiao97@gmail.com>
2020-10-25 21:33:45 +0100

    #1 Finish StoreManagerTest

--- src/test/cookies/manager/StoreManagerTest.java
+++ src/test/cookies/manager/StoreManagerTest.java
@@ -1,28 +1,23 @@
 package cookies.manager;
 
+import cookies.Store;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StoreManagerTest {
+    Store store = new Store("store1","Biot","8:00","16:00",0.15);
+    StoreManager storeManager = new StoreManager("Biot",store);
 
     @Test
-    void getTax() {
+    void changeOpenTime() {
+        storeManager.changeOpenTime("8:30");
+        assertEquals("8:30",store.getOpenTime());
     }
 
     @Test
-    void setTax() {
-    }
-
-    @Test
-    void getDay() {
-    }
-
-    @Test
-    void getOpenHour() {
-    }
-
-    @Test
-    void setOpenHour() {
+    void changeCloseTime() {
+        storeManager.changeCloseTime("16:30");
+        assertEquals("16:30",store.getCloseTime());
     }
 }
