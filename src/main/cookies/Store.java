f52f3026793cd3f637b33b085cbda281d7006af9
keyanfang <61821753+keyanfang@users.noreply.github.com>
2020-10-25 15:01:20 +0100

    #3 store update

--- src/main/cookies/Store.java
+++ src/main/cookies/Store.java
@@ -1,4 +1,31 @@
 package cookies;
 
+import cookies.recipe.Recipe;
+
 public class Store {
+    private String name;
+    private String address;
+    private float tax;
+    private int in;
+
+    public Store(String n, String a, float t, int i){
+        name=n;
+        address=a;
+        tax=t;
+        in=i;
+        }
+    public String getName(){return name;}
+    public String getAddress(){return address;}
+    public float getTax(){return tax;}
+    public int getIn(){return in;}
+    public void setIn(int t){in=t;}
+    public void setName(String n){name=n;}
+    public void setAddress(String a){address=a;}
+
+
+
+    private void makeCookies(Recipe recipe){
+
+    }
+
 }
