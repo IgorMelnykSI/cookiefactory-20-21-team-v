69825b07c376fc02fe623351e74598af149427a8
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-10-25 21:47:15 +0100

    #1 mixed cooking dough test

--- src/test/cookies/recipe/DoughTest.java
+++ src/test/cookies/recipe/DoughTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoughTest {
-
-    @Test
-    void setType() {
-    }
+    Dough plain=new Dough("Plain");
 
     @Test
     void getType() {
+        assertEquals("Plain",plain.getType());
     }
+
+    @Test
+    void setType() {
+        plain.setType("Chocolate");
+        assertEquals("Chocolate",plain.getType());
+    }
+
 }
