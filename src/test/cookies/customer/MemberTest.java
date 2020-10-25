dea24840487fde42d427d18f2e2e999cad429bb0
YUQIAO ZHAO <zhaoyuqiao97@gmail.com>
2020-10-25 21:11:53 +0100

    Revert "#1" MemberTest

--- src/test/cookies/customer/MemberTest.java
+++ src/test/cookies/customer/MemberTest.java
@@ -1,36 +1,42 @@
 package cookies.customer;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import cookies.Order;
+import cookies.customer.Member;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class MemberTest {
 
-    @Test
-    void registerLoyal() {
+public class MemberTest {
+    private Member member1;
+    private Member member2;
+
+    @BeforeEach
+    public void init(){
+        member1 = new Member("testLu");
+        member2 = new Member("trialYao");
     }
 
     @Test
-    void hasDiscount() {
+    public void registerLoyal() {
+        member1.registerLoyal();
+
+        assertTrue(member1.isLoyal());
+        assertFalse(member2.isLoyal());
     }
 
     @Test
-    void applyLoyaltyDiscount() {
+    public void applyLoyaltyDiscount() {
+        member1.registerLoyal();
+        assertEquals(member1.applyLoyaltyDiscount(),0.1,0.01);
+        assertEquals(member2.applyLoyaltyDiscount(),0,0.01);
+        assertEquals(member1.applyLoyaltyDiscount(),0,0.01);
     }
 
     @Test
-    void getId() {
-    }
-
-    @Test
-    void isLoyal() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getCookiesOrdered() {
+    public void getId() {
+        assertEquals(member1.getId() + 1, member2.getId());
+        assertTrue(member2.getId() > 0);
     }
 }
