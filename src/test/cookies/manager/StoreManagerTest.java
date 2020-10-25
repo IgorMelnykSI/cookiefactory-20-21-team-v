1fb73db49b89599ec6384c39c779751cff7a0de1
keyanfang <61821753+keyanfang@users.noreply.github.com>
2020-10-25 21:22:28 +0100

    #3 update storetest

--- src/test/cookies/manager/StoreManagerTest.java
+++ src/test/cookies/manager/StoreManagerTest.java
@@ -1,28 +1,34 @@
 package cookies.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class StoreManagerTest {
+import static org.junit.Assert.*;
 
-    @Test
-    void getTax() {
+class StoreTest {
+    private StoreManager s1;
+    private StoreManager s2;
+
+    @BeforeEach
+    public void init(){
+        s1=new StoreManager("store1","address1",10,1);
+        s2 = new StoreManager("store2","address2",50,1);
     }
 
     @Test
-    void setTax() {
+    public void setTax(){
+        s1.setTax(20);
+        assertEquals(s1.getTax(),20,0.01);
     }
-
     @Test
-    void getDay() {
+    public void getHour(){
+        assertEquals(s1.getOpenHour(4),"8:00-16:30");
+        assertEquals(s2.getOpenHour(10),"wrong day please try again");
     }
-
     @Test
-    void getOpenHour() {
-    }
-
-    @Test
-    void setOpenHour() {
+    public void setHour(){
+        s2.setOpenHour(0,"6:00-17:00");
+        assertEquals(s2.getOpenHour(0),"6:00-17:00");
     }
 }
