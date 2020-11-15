ed11e6980ac67c84973933c267c66346b33ed6ed
YaoLu314 <503372430@qq.com>
2020-11-15 22:07:03 +0100

    compare recipe #12

--- src/test/java/recipe/RecipeTest.java
+++ src/test/java/recipe/RecipeTest.java
@@ -1,20 +1,34 @@
 package recipe;
 
 import cookies.recipe.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RecipeTest {
-    Recipe recipe1=new Recipe("recipe1");//5.5
-    Cooking crunchy=new Cooking("Crunchy");
-    Flavour vanilla=new Flavour("Vanilla");
-    Dough peabut=new Dough("Peanut butter");
-    Mix mixed=new Mix("Mixed");
-    Topping mm=new Topping("M&M’s™");
-    Topping reese=new Topping("Reese’s buttercup");
-    Topping[] tops1=new Topping[]{mm,reese};
-    Topping[] tops2=new Topping[]{reese};
+    Recipe recipe1;//5.5
+    Cooking crunchy;
+    Flavour vanilla;
+    Dough peabut;
+    Mix mixed;
+    Topping mm;
+    Topping reese;
+    Topping[] tops1;
+    Topping[] tops2;
+
+    @BeforeEach
+    public void init(){
+        recipe1=new Recipe("recipe1");//5.5
+        crunchy=new Cooking("Crunchy");
+        vanilla=new Flavour("Vanilla");
+        peabut=new Dough("Peanut butter");
+        mixed=new Mix("Mixed");
+        mm=new Topping("M&M’s™");
+        reese=new Topping("Reese’s buttercup");
+        tops1=new Topping[]{mm,reese};
+        tops2=new Topping[]{reese};
+    }
 
     @Test
     void getName() {
@@ -44,6 +58,24 @@ class RecipeTest {
     }
 
     @Test
+    void compareRecipe() {
+        recipe1.setCooking(crunchy);
+        recipe1.setFlavour(vanilla);
+        recipe1.setDough(peabut);
+        recipe1.setMix(mixed);
+        recipe1.setToppings(tops1);
+
+        Recipe recipe2=new Recipe("recipe2");
+        recipe2.setCooking(crunchy);
+        recipe2.setFlavour(vanilla);
+        recipe2.setDough(peabut);
+        recipe2.setMix(mixed);
+        recipe2.setToppings(tops1);
+
+        assertTrue(recipe1.compareRecipe(recipe2));
+    }
+
+    @Test
     void setPrice() {
         recipe1.setCooking(crunchy);
         recipe1.setFlavour(vanilla);
@@ -69,25 +101,25 @@ class RecipeTest {
     @Test
     void setCooking() {
         recipe1.setCooking(crunchy);
-        assertEquals(crunchy,recipe1.getCooking());
+        assertTrue(crunchy.getType().equals(recipe1.getCooking().getType()));
     }
 
     @Test
     void setFlavour() {
         recipe1.setFlavour(vanilla);
-        assertEquals(vanilla,recipe1.getFlavour());
+        assertTrue(vanilla.getType().equals(recipe1.getFlavour().getType()));
     }
 
     @Test
     void setDough() {
         recipe1.setDough(peabut);
-        assertEquals(peabut,recipe1.getDough());
+        assertTrue(peabut.getType().equals(recipe1.getDough().getType()));
     }
 
     @Test
     void setMix() {
         recipe1.setMix(mixed);
-        assertEquals(mixed,recipe1.getMix());
+        assertTrue(mixed.getType().equals(recipe1.getMix().getType()));
     }
 
     @Test
