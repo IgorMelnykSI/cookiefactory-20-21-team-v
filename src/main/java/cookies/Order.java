aafc1df6f24c7fcf5e8bbe63fb8990537c4fec7c
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-11-29 15:50:15 +0100

    #14

--- src/main/java/cookies/Order.java
+++ src/main/java/cookies/Order.java
@@ -95,19 +95,19 @@ public class Order {
         }
 
         if(store.hasProblem()){
-            setState(new FailState());
+            //setState(new FailState());
             throw new MyException("The store has technical problems, please choose another store\n");
         }
         if(store.isBusy(date)){
-            setState(new FailState());
+            //setState(new FailState());
             throw new MyException("The store is busy, please choose another store\n");
         }
-        if(store.checkOrder(this)){
-            setState(new FailState());
-            throw new MyException("There is not enough ingredients");
-        }
+        //if(store.checkOrder(this)){
+            //setState(new FailState());
+        //    throw new MyException("There is not enough ingredients");
+        //}
 
-        setState(new ConfirmState());
+        //setState(new ConfirmState());
     }
 
     public  String getTheWay(){
